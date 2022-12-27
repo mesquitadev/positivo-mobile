71bcfa14eff90ed18d60ef617045ec8c191195fb
Paulo Victor Mesquita <mesquitadev@gmail.com>
2022-12-27 09:50:13 -0300

    feat: :tada: create initial structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Montserrat_100Thin,
+  Montserrat_300Light,
+  Montserrat_200ExtraLight,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+  useFonts,
+} from '@expo-google-fonts/montserrat';
+import { DropDownComponentProvider } from '@hooks/dropdown';
+import AppProvider from '@hooks/index';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from '@routes/index';
+import theme from '@theme/index';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_100Thin,
+    Montserrat_300Light,
+    Montserrat_200ExtraLight,
+    Montserrat_400Regular,
+    Montserrat_700Bold,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_800ExtraBold,
+    Montserrat_900Black,
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DropDownComponentProvider>
+      <ThemeProvider theme={theme}>
+        <View style={styles.container}>
+          <NavigationContainer>
+            <AppProvider>
+              {fontsLoaded ? (
+                <View style={styles.container}>
+                  <Routes />
+                </View>
+              ) : (
+                <ActivityIndicator color="red" size={30} />
+              )}
+            </AppProvider>
+          </NavigationContainer>
+        </View>
+      </ThemeProvider>
+    </DropDownComponentProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './assets',
+            '@components': './src/components',
+            '@contexts': './src/contexts',
+            '@hooks': './src/hooks',
+            '@pages': './src/pages',
+            '@routes': './src/routes',
+            '@services': './src/services',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
